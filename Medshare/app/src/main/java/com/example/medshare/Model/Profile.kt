6af18620c1bd4706afce7d82ac4f74ef9787adbe
sandippan52 <sandippan52@gmail.com>
2025-09-03 23:23:32 +0530

    Restructured  models

---
+++ Medshare/app/src/main/java/com/example/medshare/Model/Profile.kt
@@ -0,0 +1,8 @@
+package com.example.medshare.Model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Profile(
+    @StringRes val stringResourceId : Int
+)
